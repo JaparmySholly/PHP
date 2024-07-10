baf6bdc0e2f2abc3f92a27687c42b25db5345d1d
JaparmySholly <japarmysholly@gmail.com>
2024-07-10 13:05:08 +0100

    functions $ array

---
+++ function.php
@@ -0,0 +1,10 @@
+    <?php
+    function familyName($fname, $year) {
+        echo "$fname Refsnes. Born in $year <br>";
+      }
+      
+      familyName("Jani", "2010");
+      familyName("Hege", "2012");
+      familyName("Stale", "2011");
+      familyName("Kai Jim", "2009");
+      familyName("Borge", "2008");
