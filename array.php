5a01b407d3cfde5cb32fc9d67b69a76e9e453316
JaparmySholly <japarmysholly@gmail.com>
2024-07-10 14:52:01 +0100

    array

--- array.php
+++ array.php
@@ -1,4 +1,11 @@
 <?php
 
-$cars = array("Volvo", "BMW", "Toyota");
-$cars[1] = "Ford";
+// Looping through an Associative Array;
+
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+
+foreach ($car as $x => $y) {
+    echo "$x: $y <br>";
+    
+}
+
