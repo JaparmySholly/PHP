6a299ddaeb9424676eb28423ad582105353bf090
JaparmySholly <japarmysholly@gmail.com>
2024-07-11 12:01:09 +0100

    get_post

---
+++ get_post.php
@@ -0,0 +1,6 @@
+<?php
+echo $_GET("name");
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?
+name=Sholly">Click</a>
--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 
+$people = [
+    [
+    'first_name' => 'Japarmy',
+    'last_name' => 'Sholly',
+    'email' => 'japarmy@gmail.com',
+    ],
+    [
+    'first_name' => 'John',
+    'last_name' => 'Lizzy',
+    'email' => 'shola@gmail.com',
+    ],
+    [
+    'first_name' => 'Abas',
+    'last_name' => 'Nelson',
+    'email' => 'Hope@gmail.com',
+    ],
+];
 
-
-?>
-
-</body>
-</html>
+echo $people[1]['email'];
