b584945c97d8e1b15dc9781000e2c1ca87614bc0
JaparmySholly <japarmysholly@gmail.com>
2024-07-11 15:50:20 +0100

    dashboard

--- session.php
+++ session.php
@@ -1,2 +1,28 @@
 <?php
+session_start();
 
+if (isset($_POST['submit'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if ($username === 'john' && $password === 'password') {
+        $_SESSION['username'] = $username;
+        header('Location: /dashboard.php');
+        exit(); // Make sure to exit after a redirect to stop further execution
+    } else {
+        echo 'Incorrect Login';
+    }
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
