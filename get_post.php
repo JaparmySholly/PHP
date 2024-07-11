beeefbebf2cbf0b2e49fd9531bb1c731b9d087f5
JaparmySholly <japarmysholly@gmail.com>
2024-07-11 13:27:29 +0100

    post

--- get_post.php
+++ get_post.php
@@ -1,6 +1,9 @@
 <?php
-echo $_GET("name");
+if (isset($_GET['name'])) {
+    echo htmlspecialchars($_GET['name']);
+} else {
+    echo 'Name parameter is missing.';
+}
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>?
-name=Sholly">Click</a>
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Sholly">Click</a>
